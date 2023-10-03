281db2172cf03464ee343e78441d6006928954a4
nightrobin <thenightrobin@gmail.com>
2023-10-03 12:11:21 +0800

    Added env file reading

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"log"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	ex, err := os.Executable()
+    if err != nil {
+        panic(err)
+    }
+    exPath := filepath.Dir(ex)
+
+	err = godotenv.Load(exPath + "/.env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	fmt.Println(os.Getenv("DB_HOST"))
+	fmt.Println(os.Getenv("DB_PORT"))
+	fmt.Println(os.Getenv("DB_NAME"))
+	fmt.Println(os.Getenv("DB_USER"))
+	fmt.Println(os.Getenv("DB_PASS"))
+}
