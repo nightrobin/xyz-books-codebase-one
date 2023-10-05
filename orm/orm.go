a80b189bdf959b96a3afc97385a0a6b425281666
nightrobin <thenightrobin@gmail.com>
2023-10-05 13:04:49 +0800

    Fixed ORM connection string

--- orm/orm.go
+++ orm/orm.go
@@ -12,7 +12,7 @@ import (
 var db *gorm.DB
 
 func ConnectToDB() *gorm.DB {
-	var config string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&allowNativePasswords=True", os.Getenv("G12PH_DB_USER"), os.Getenv("G12PH_DB_PASS"), os.Getenv("G12PH_DB_HOST"), os.Getenv("G12PH_DB_PORT"), os.Getenv("G12PH_DB_NAME"))
+	var config string = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&allowNativePasswords=True", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 	db, err := gorm.Open(mysql.New(mysql.Config{
 		DSN:               config,
 		DefaultStringSize: 256,
