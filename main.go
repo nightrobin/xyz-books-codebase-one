6e711e121db8f1a71094f2a106de4518d93ac2ed
nightrobin <thenightrobin@gmail.com>
2023-10-05 12:02:39 +0800

    Added initial index page

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	// "fmt"
+    // "bytes"
+	"html/template"
 	"os"
 	"log"
+	"net/http"
 	"path/filepath"
+
 
 	"github.com/joho/godotenv"
 
@@ -12,6 +16,7 @@ import (
 )
 
 func main() {
+	// Load Environment Variables
 	ex, err := os.Executable()
     if err != nil {
         panic(err)
@@ -23,11 +28,43 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	// fmt.Println(os.Getenv("DB_HOST"))
-	// fmt.Println(os.Getenv("DB_PORT"))
-	// fmt.Println(os.Getenv("DB_NAME"))
-	// fmt.Println(os.Getenv("DB_USER"))
-	// fmt.Println(os.Getenv("DB_PASS"))
+	// Apply Migrations
 	dbmigration.ApplyMigrations()
-	
+
+
+	// Index.html
+	parsedIndexTemplate, err := template.ParseFiles(exPath + "/templates/index.html")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+	tmpl := template.Must(parsedIndexTemplate, err)
+
+	type PageData struct {
+		Title string
+		Content string
+	}
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        data := PageData{
+            Title:   "Page Title",
+            Content: "Hello, World!",
+        }
+
+        // Set the Content-Type header to indicate that we are sending HTML.
+        w.Header().Set("Content-Type", "text/html; charset=utf-8")
+
+        // Execute the template and send the result to the client.
+        if err := tmpl.Execute(w, data); err != nil {
+            http.Error(w, err.Error(), http.StatusInternalServerError)
+        }
+    })
+
+
+	log.Print("Listening on :3000...")
+	err = http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
