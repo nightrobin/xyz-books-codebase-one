1e928040c54b97a33f41f4e8b0b2ce98d91f7660
nightrobin <thenightrobin@gmail.com>
2023-10-10 21:17:39 +0800

    Added Image URL for some of the books

--- migrations/5_insert_database_data.up.sql
+++ migrations/5_insert_database_data.up.sql
@@ -11,11 +11,11 @@ INSERT INTO `publishers` (`id`, `name`) VALUES ('2', 'Publishers Weekly');
 INSERT INTO `publishers` (`id`, `name`) VALUES ('3', 'Graywolf Press');
 INSERT INTO `publishers` (`id`, `name`) VALUES ('4', 'McSweeney\'s');
 
-INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `edition`, `list_price`) VALUES ('1', 'American Elf', '9781891830853', '1891830856', '2004', '1', 'Book 2', '1000.00');
+INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `edition`, `list_price`, `image_url`) VALUES ('1', 'American Elf', '9781891830853', '1891830856', '2004', '1', 'Book 2', '1000.00', 'https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg');
 INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `edition`, `list_price`) VALUES ('2', 'Cosmoknights', '9781603094542', '1603094547', '2019', '2', 'Book 1', '2000.00');
-INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `list_price`) VALUES ('3', 'Essex County', '9781603090384', '160309038X', '1990', '3', '500.00');
+INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `list_price`, `image_url`) VALUES ('3', 'Essex County', '9781603090384', '160309038X', '1990', '3', '500.00', 'https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg');
 INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `edition`, `list_price`) VALUES ('4', 'Hey, Mister (Vol 1)', '9781891830020', '1891830023', '2000', '3', 'After School Special', '1200.00');
-INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `list_price`) VALUES ('5', 'The Underwater Welder', '9781603093989', '1603093982', '2022', '4', '3000.00');
+INSERT INTO `books` (`id`, `title`, `isbn_13`, `isbn_10`, `publication_year`, `publisher_id`, `list_price`, `image_url`) VALUES ('5', 'The Underwater Welder', '9781603093989', '1603093982', '2022', '4', '3000.00', 'https://www.collinsdictionary.com/images/full/book_181404689_1000.jpg');
 
 INSERT INTO `book_authors` (`id`, `book_id`, `author_id`) VALUES ('1', '1', '1');
 INSERT INTO `book_authors` (`id`, `book_id`, `author_id`) VALUES ('2', '1', '2');
