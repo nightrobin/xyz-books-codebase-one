dcd010383a4cd723aa3b5c736c516082ea211ba6
nightrobin <thenightrobin@gmail.com>
2023-10-13 03:13:42 +0800

    Fixed duplicated ISBN 13

--- model/book.go
+++ model/book.go
@@ -5,7 +5,7 @@ type Book struct {
 	Title			string		`form:"Title" json:"Title" validate:"required"`
 	Isbn13			string		`form:"Isbn13" gorm:"column:isbn_13" json:"Isbn13"`
 	Isbn10			string		`form:"Isbn10" gorm:"column:isbn_10" json:"Isbn10"`
-	PublicationYear	int16 		`form:"PublicationYear" json:"PublicationYear" validate:"required"`
+	PublicationYear	int16 		`form:"PublicationYear" json:"PublicationYear" validate:"required,number,min=1000,max=2023"`
 	PublisherID		uint64		`form:"PublisherID" json:"PublisherID" validate:"required"`
 	ImageURL		string		`form:"ImageURL" json:"ImageURL"`
 	Edition			string		`form:"Edition" json:"Edition"`
